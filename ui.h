0d1a7e82db7b4b5a65abe1e6a5a8a8a9a96612fc
Somk1ng <148505779+Somk1ng@users.noreply.github.com>
2024-02-29 22:53:19 +0700

    Fixing compiler warning:

---
+++ ui.h
@@ -0,0 +1,8 @@
+
+/*Display the board on the game*/
+#include"types.h"
+void display(int arr[][3],char p[]);
+void showturn(int p);
+void inputPosition(Position *pos);
+void showGameResult(int arr[][3]);
+void alert(const char *s);
