835eadc80a59226e05b43846d20ec79f833b83f2
Somk1ng <148505779+Somk1ng@users.noreply.github.com>
2024-02-29 22:51:04 +0700

    Failed attempt at early draw detection

---
+++ status.h
@@ -0,0 +1,13 @@
+
+/*Returns the winner if winner can be determined
+**ie. 1 or 2 indicating player 1 or player 2
+**else 0 if result undetermined or game drawn
+*/
+int win(int arr[][3]);
+/*Returns 1 if game drawn*/
+int draw(int arr[][3]);
+
+/*Checks if any player has a chance of winning*/
+int winPossible(int arr[][3]);
+
+int filled(int arr[][3]);
